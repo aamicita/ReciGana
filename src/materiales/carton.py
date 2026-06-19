b7127cb40c6c05c7892ce94bc3082522c8b6dd39
yomaira-vp <yomairavapi2112@gmail.com>
2026-06-19 13:32:44 -0500

    @classmethod
    Solo le faltaba acomodar, le faltaba espacio

--- src/materiales/carton.py
+++ src/materiales/carton.py
@@ -11,7 +11,7 @@ class Carton(MaterialBase):
 
     PRECIO_BASE = 0.25
  
-     @classmethod
+    @classmethod
     def cambiar_precio_base(cls, nuevo_precio: float):
         if not isinstance(nuevo_precio, (int, float)) or nuevo_precio <= 0:
             raise ValueError("El precio debe ser mayor a cero.")
